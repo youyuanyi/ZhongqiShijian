417cd18effbe71f20d61bb4fbfaf75e9ae995fc2
youyuanyi <67009919+youyuanyi@users.noreply.github.com>
2022-04-25 18:49:53 +0800

    Update settings.py

--- Web/ZhongqiShijian/ZhongqiShijian/settings.py
+++ Web/ZhongqiShijian/ZhongqiShijian/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru%&73k2jez(s*5m(bm$od#0t=8ru=h9%(%*7lcm-wjj0!v80f'
+# SECRET_KEY = ''  这里引号中输入你的Django的key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
